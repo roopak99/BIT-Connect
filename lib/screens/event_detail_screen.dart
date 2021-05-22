4ba5f70ab5949ea7bed7a7b0106e63682ab430f6
Roopak Bedekar <roopakbedekar@gmail.com>
2021-05-22 17:31:04 +0530

    event detail screen redone

--- lib/screens/event_detail_screen.dart
+++ lib/screens/event_detail_screen.dart
@@ -15,15 +15,109 @@ class EventDetail extends StatelessWidget {
         centerTitle: true,
         title: Text(loadedEvent.title),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(child: Text(loadedEvent.title)),
-          Center(child: Text(loadedEvent.description)),
-          Center(child: Text(loadedEvent.branch)),
-          Center(child: Text(loadedEvent.batch)),
-          Center(child: Text(loadedEvent.eventDate.toIso8601String())),
-        ],
+      body: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 80,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                    ),
+                    child: Icon(Icons.info_rounded),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                    ),
+                    child: Text(
+                      loadedEvent.title,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            SizedBox(
+              height: 100,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8,
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.calendar_view_day_rounded),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 5),
+                              child: Text(loadedEvent.batch),
+                            ),
+                          ],
+                        ),
+                        Divider(),
+                        Row(
+                          children: [
+                            Icon(Icons.people_rounded),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Text(loadedEvent.branch),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: VerticalDivider(),
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.calendar_today_rounded),
+                              Text('Event Date'),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 30),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.alarm),
+                              Text("Time left"),
+                              Text(loadedEvent.eventDate
+                                  .difference(DateTime.now())
+                                  .toString()),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Divider(),
+            SelectableText(loadedEvent.description),
+          ],
+        ),
       ),
     );
   }
